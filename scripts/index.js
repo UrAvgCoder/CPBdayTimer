dba66ceab76520ce5b5da951c2812773e006a37d
UrAvgCoder <niri1607@gmail.com>
2020-11-04 23:38:40 +0530

    next year bday

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Nov 04, 2020',
+  birthdate: 'Nov 04, 2021',
   name: 'CP!'
 };
 
